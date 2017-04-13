30c889dba2e6645c90568302b272bbb006867aa5
Nikita Kirilov <kirilov.dev@gmail.com>
2017-04-14 01:06:44 +0700

    add integer types support

--- lexems_automats/base.py
+++ lexems_automats/base.py
@@ -2,12 +2,19 @@ __author__ = 'Nikita'
 
 
 def is_digit(char):
-    a = '0' <= char <= '9'
     return '0' <= char <= '9'
 
 
+def is_bin_digit(char):
+    return '0' <= char <= '1'
+
+
+def is_oct_digit(char):
+    return '0' <= char <= '7'
+
+
 def is_hex_digit(char):
-    return 'a' <= char <= 'f'
+    return '0' <= char <= '9' or 'a' <= char <= 'f'
 
 
 def is_letter(char):
--- lexems_automats/numbers.py
+++ lexems_automats/numbers.py
@@ -1,88 +1,98 @@
 __author__ = 'Nikita'
 
-from lexems_automats.base import is_digit, is_hex_digit, is_letter
+from lexems_automats.base import is_digit, is_bin_digit, is_oct_digit, is_hex_digit
+from lexems_automats.base import is_letter
 
 
-def Is_dec_int(scanner_params):
+def Is_bin_int(scaner_maras):
+    file, _, _, _, _ = scaner_maras
 
-    file, _, _, _, _ = scanner_params
-
-    local_offset = 1
     char = file.read(1)
-    if is_digit(char):
+    local_offset = 1
+    if is_bin_digit(char):
         while True:
-            char = file.read(1)
-            if is_digit(char):
-                local_offset += 1
-            else:
-                break
-        if not is_letter(char):
-            return True, {'offset': local_offset, 'lexeme': 'DecInt'}
+            char = file.read(1).lower()
+            if not is_bin_digit(char):
+                if char == 'b':
+                    local_offset += 1
+                    break
+                else:
+                    return False, {'error': "Bin integer's form is wrong"}
+            local_offset += 1
+        char = file.read(1)
+        if not (is_letter(char) and is_digit(char)):
+            return True, {'offset': local_offset, 'lexeme': 'BinInt'}
         else:
-            return False, {}
+            return False, {'error': "Bin integer's form is wrong"}
     else:
         return False, {}
 
-#
-# def is_bin_int(chars):
-#     chars = chars.lower()
-#
-#     # pattern = r'b'
-#     # if not re.match(pattern, chars):
-#     if not chars.endswith("b"):
-#         return False
-#
-#     chars = chars.replace('b', '')
-#     for ch in chars:
-#         if ch < "0" or ch > "1":
-#             return False
-#     return True
-#
-#
-# def is_oct_int(chars):
-#     chars = chars.lower()
-#     if not chars.endswith("c"):
-#         return False
-#
-#     chars = chars.replace('c', '')
-#     for ch in chars:
-#         if ch < "0" or ch > "7":
-#             return False
-#     return True
-#
-#
-# # def is_dec_int(chars):
-# #     chars = chars.lower()
-# #     if chars.endswith('d'):
-# #         chars = chars.replace('d', '')
-# #
-# #     for ch in chars:
-# #         if ch < "0" or ch > "9":
-# #             return False
-# #     return True
-#
-#
-# def is_hex_int(chars):
-#     chars = chars.lower()
-#     if chars.endswith('h'):
-#         return False
-#
-#     chars = chars.replace('h', '')
-#     for ch in chars:
-#         if not ('0' <= ch <= '9' or 'a' <= ch <= 'f'):
-#             return False
-#     return True
-#
-#
-# # REAL
-# def is_real(chars):
-#     pattern_one = r'\d*e(\+|\-)?\d+$'
-#     pattern_two = r'\d*.\d*(e(\+|\-)?\d+)?$'
-#     if re.match(pattern_one, chars):
-#         # print('pattern one')
-#         return True
-#     elif re.match(pattern_two, chars):
-#         # print('pattern two')
-#         return True
-#     else:
-#         return False
+
+def Is_oct_int(scanner_params):
+    file, _, _, _, _ = scanner_params
+
+    char = file.read(1).lower()
+    local_offset = 1
+    if is_oct_digit(char):
+        while True:
+            char = file.read(1).lower()
+            if not is_oct_digit(char):
+                if char == 'c':
+                    local_offset += 1
+                    break
+                else:
+                    return False, {'error': "Oct integer's form is wrong"}
+            local_offset += 1
+        char = file.read(1)
+        if not (is_letter(char) and is_digit(char)):
+            return True, {'offset': local_offset, 'lexeme': 'OctInt'}
+        else:
+            return False, {'error': "OctInt integer's form is wrong"}
+    else:
+        return False, {}
+
+
+def Is_dec_int(scanner_params):
+    file, _, _, _, _ = scanner_params
+
+    char = file.read(1)
+    local_offset = 1
+    if is_digit(char):
+        while True:
+            char = file.read(1)
+            if not is_digit(char):
+                if char == 'd':
+                    local_offset += 1
+                break
+            else:
+                local_offset += 1
+        if not (is_letter(char) and is_digit(char)):
+            return True, {'offset': local_offset, 'lexeme': 'DecInt'}
+        else:
+            return False, {'error': "Dec integer's form is wrong"}
+    else:
+        return False, {}
+
+
+def Is_hex_int(scanner_params):
+    file, _, _, _, _ = scanner_params
+
+    char = file.read(1).lower()
+    local_offset = 1
+    if is_hex_digit(char):
+        while True:
+            char = file.read(1).lower()
+            if not is_hex_digit(char):
+                if char == 'h':
+                    local_offset += 1
+                    break
+                else:
+                    return False, {'error': "Hex integer's form is wrong"}
+            local_offset += 1
+        char = file.read(1)
+        if not (is_letter(char) and is_digit(char)):
+            return True, {'offset': local_offset, 'lexeme': 'HexInt'}
+        else:
+            return False, {'error': "HexInt integer's form is wrong"}
+    else:
+        return False, {}

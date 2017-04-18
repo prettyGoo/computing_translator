5cf5c590eea63fc7ea42529efba78b6fc5075ca9
Nikita Kirilov <kirilov.dev@gmail.com>
2017-04-18 19:00:27 +0700

    fix some output. ISSUE: see code.txt

--- lexems_printer.py
+++ lexems_printer.py
@@ -26,12 +26,14 @@ def print_lexeme(output_file, row, lexeme, value, error_message):
             output_file.write('{}\tlex:{}\tint:{}\tval:{}\n'.format(row, lexeme, value, int(value, digit_base)))
         elif lexeme == 'Error':
             output_file.write('{}\tlex:{}\tval:{}\n'.format(row, lexeme, value))
-            print(error_message)
+            print('{}:{}'.format(row, error_message))
             error_counter += 1
         elif lexeme == 'EOF':
             output_file.write('{}\tlex:{}\tval:{}\n'.format(row, lexeme, value))
             if error_counter == 0:
                 print('OK')
+        elif lexeme == 'Space' or lexeme == 'Tab' or lexeme == 'NewLine':
+            return
         else:
             output_file.write('{}\tlex:{}\tval:{}\n'.format(row, lexeme, value))
     else:
